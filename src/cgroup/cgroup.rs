167b51b6b19dc7222620eae9b683a564f04486ae
mii443 <mii@sfc.wide.ad.jp>
2024-10-17 01:56:45 +0000

    add read fn

--- src/cgroup/cgroup.rs
+++ src/cgroup/cgroup.rs
@@ -1,5 +1,6 @@
 use std::{
-    fs,
+    fs::{self, File},
+    io::Read,
     path::{Path, PathBuf},
 };
 
@@ -23,6 +24,15 @@ impl CGroup {
     fn create(&self) -> Result<(), std::io::Error> {
         let root = self.get_root_path();
         fs::create_dir_all(root)
+    }
+
+    pub fn read(&self, name: &str) -> Result<String, std::io::Error> {
+        let path = self.get_file_path(name);
+        let mut file = File::open(path)?;
+        let mut buf = String::default();
+        file.read_to_string(&mut buf)?;
+
+        Ok(buf)
     }
 
     pub fn check_status(&self) -> bool {
