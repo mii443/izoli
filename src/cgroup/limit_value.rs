49b12fd2965c89143af12a443b79fde1cdba6ae4
mii443 <mii@sfc.wide.ad.jp>
2024-10-17 07:56:13 +0000

    impl FromStr for CGroupLimitValue

--- src/cgroup/limit_value.rs
+++ src/cgroup/limit_value.rs
@@ -1,5 +1,32 @@
+use std::str::FromStr;
+
 #[derive(Debug)]
-pub enum CGroupLimitValue<T> {
+pub enum CGroupLimitValue<T>
+where
+    T: FromStr,
+{
     Max,
     Value(T),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseCGroupLimitValueError;
+
+impl<T> FromStr for CGroupLimitValue<T>
+where
+    T: FromStr,
+{
+    type Err = ParseCGroupLimitValueError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.trim() == "max" {
+            Ok(Self::Max)
+        } else {
+            if let Ok(value) = T::from_str(s) {
+                Ok(Self::Value(value))
+            } else {
+                Err(ParseCGroupLimitValueError)
+            }
+        }
+    }
 }
