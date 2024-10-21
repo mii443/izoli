5086fe0ae72db9f7731341f2279bb4ba50ce7af1
Masato Imai <mii@sfc.wide.ad.jp>
2024-10-21 02:02:24 +0000

    add box enter

--- src/izolibox.rs
+++ src/izolibox.rs
@@ -1,1 +1,25 @@
+use nix::{
+    libc::SIGCHLD,
+    sched::{self, CloneCb, CloneFlags},
+    unistd::Pid,
+};
+
+const STACK_SIZE: usize = 8192;
+
 pub struct IzoliBox {}
+
+impl IzoliBox {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn enter(&self, callback: CloneCb<'_>) -> Result<Pid, nix::errno::Errno> {
+        let mut stack = [0u8; STACK_SIZE];
+        let flags = CloneFlags::CLONE_NEWNS
+            | CloneFlags::CLONE_NEWUTS
+            | CloneFlags::CLONE_NEWIPC
+            | CloneFlags::CLONE_NEWPID;
+
+        unsafe { sched::clone(callback, &mut stack, flags, Some(SIGCHLD)) }
+    }
+}
